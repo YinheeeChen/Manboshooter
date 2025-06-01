fa29cbb4cd01f0b769d1847718355375673a85ca
YinheeeChen <yinhechen@csu.edu.cn>
2025-06-01 15:27:12 +0800

    wave manage

--- Assets/Kawaii Survivor/Scripts/Managers/WaveManger.cs
+++ Assets/Kawaii Survivor/Scripts/Managers/WaveManger.cs
@@ -2,22 +2,58 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 
 public class WaveManger : MonoBehaviour
 {
-    [Header("Elements")]
-    [SerializeField] private Wave[] wave;
+    [Header("Settings")]
+    [SerializeField] private float waveDuration;
+    private float timer;
+
+    [Header("Waves")]
+    [SerializeField] private Wave[] waves;
+    private List<float> localCounters = new List<float>();
 
     // Start is called before the first frame update
     void Start()
     {
-
+        localCounters.Add(1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (timer < waveDuration)
+            ManageCurrentWave();
+    }
 
+    private void ManageCurrentWave()
+    {
+        Wave currentWave = waves[0];
+
+        for (int i = 0; i < currentWave.segments.Count; i++)
+        {
+            WaveSegment segment = currentWave.segments[i];
+
+            float tStart = segment.tStartEnd.x / 100 * waveDuration;
+            float tEnd = segment.tStartEnd.y / 100 * waveDuration;
+
+            if (timer < tStart || timer > tEnd)
+                continue;
+
+            float timeSinceSegmentStart = timer - tStart;
+
+            float spawnDelay = 1f / segment.spawnFrequency;
+
+            if (timeSinceSegmentStart / spawnDelay >= localCounters[i])
+            {
+                Instantiate(segment.prefab, Vector2.zero, Quaternion.identity, transform);
+                localCounters[i]++;
+            }
+
+        }
+        
+        timer += Time.deltaTime;
     }
 }
 
@@ -31,6 +67,8 @@ public struct Wave
 [System.Serializable]
 public struct WaveSegment
 {
+    [MinMaxSlider(0, 100)] public Vector2 tStartEnd;
+    public float spawnFrequency;
     public GameObject prefab;
-    
+
 }
