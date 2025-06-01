a9d6f8df540fc19124acb4293b195207e596dfb0
YinheeeChen <yinhechen@csu.edu.cn>
2025-06-02 00:20:19 +0800

    add upgrade container

--- Assets/Kawaii Survivor/Scripts/Enums.cs
+++ Assets/Kawaii Survivor/Scripts/Enums.cs
@@ -1,3 +1,5 @@
+using Unity.Collections.LowLevel.Unsafe;
+
 public enum GameState
 {
     MENU,
@@ -7,4 +9,40 @@ public enum GameState
     STAGECOMPLETE,
     WAVETRANSITION,
     SHOP
+}
+
+public enum Stat
+{
+    Attack,
+    AttackSpeed,
+    CriticalChance,
+    CriticalPercent,
+    MoveSpeed,
+    MaxHealth,
+    Range,
+    HealthRecoverySpeed,
+    Armor,
+    Luck,
+    Dodge,
+    LifeSteal
+}
+
+public static class Enums
+{
+    public static string FormatStatName(Stat stat)
+    {
+        string formated = "";
+        string unformatedString = stat.ToString();
+
+        for (int i = 0; i < unformatedString.Length; i++)
+        {
+            if (i > 0 && char.IsUpper(unformatedString[i]))
+                formated += " ";
+
+            formated += unformatedString[i];
+        }
+
+        return formated;
+
+    }
 }
---
+++ Assets/Kawaii Survivor/Scripts/Managers/WaveTransitionManager.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using NaughtyAttributes;
+
+using Random = UnityEngine.Random;
+
+public class WaveTransitionManager : MonoBehaviour, IGameStateListener
+{
+    [Header("Elements")]
+    [SerializeField] private Button[] upgradeContainers;
+    private WaveManagerUI ui;
+
+    [Header("Settings")]
+    [SerializeField] private float waveDuration;
+    private float timer;
+    private bool isTimerOn;
+    private int currentWaveIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GmaeStateChangeCallback(GameState gameState)
+    {
+        switch (gameState)
+        {
+            case GameState.WAVETRANSITION:
+                ConfigureUpgradeContainers();
+                break;
+        }
+    }
+
+    [Button]
+    private void ConfigureUpgradeContainers()
+    {
+        for (int i = 0; i < upgradeContainers.Length; i++)
+        {
+            int randomIndex = Random.Range(0, Enum.GetValues(typeof(Stat)).Length);
+            Stat stat = (Stat)Enum.GetValues(typeof(Stat)).GetValue(randomIndex);
+            string randomStatString = Enums.FormatStatName(stat);
+
+            upgradeContainers[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = randomStatString;
+            upgradeContainers[i].onClick.RemoveAllListeners();
+            upgradeContainers[i].onClick.AddListener(() =>
+            {
+                // Here you would apply the upgrade logic, e.g.:
+                // player.UpgradeStat(stat);
+                Debug.Log("Upgrading " + randomStatString);
+            });
+        }
+    }
+}
