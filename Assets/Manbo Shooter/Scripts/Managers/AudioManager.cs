afdb053ccb69e92e08a7ed950827d0691929e8c1
YinheeeChen <yinhechen@csu.edu.cn>
2025-06-11 19:20:33 +0800

    rename

--- Assets/Manbo Shooter/Scripts/Managers/AudioManager.cs
+++ Assets/Manbo Shooter/Scripts/Managers/AudioManager.cs
@@ -9,13 +9,20 @@ public class AudioManager : MonoBehaviour
 
     public bool IsSFXOn { get; private set; }
     public bool IsMusicOn { get; private set; }
+    private int lastIndex = -1;
 
     private void Awake()
     {
         if (instance == null)
+        {
             instance = this;
+            // DontDestroyOnLoad(gameObject);
+        }
+            
         else
             Destroy(gameObject);
+
+
 
         SettingsManager.onSFXStateChanged += SFXStateChangedCallback;
         SettingsManager.onMusicStateChanged += MusicStateChangedCallback;
@@ -31,9 +38,39 @@ public class AudioManager : MonoBehaviour
     {
         IsSFXOn = sfxState;
     }
-    
+
     private void MusicStateChangedCallback(bool musicState)
     {
         IsMusicOn = musicState;
+
+        AudioSource[] audioSources = GetComponentsInChildren<AudioSource>();
+
+        if (musicState)
+        {
+            lastIndex = (lastIndex + 1) % audioSources.Length;
+            for (int i = 0; i < audioSources.Length; i++)
+            {
+                if (i == lastIndex)
+                {
+                    audioSources[i].mute = false;
+                    audioSources[i].Stop();
+                    audioSources[i].Play();
+                }
+                else
+                {
+                    audioSources[i].Stop();
+                    audioSources[i].mute = true;
+                }
+            }
+        }
+        else
+        {
+
+            foreach (var audioSource in audioSources)
+            {
+                audioSource.Stop();
+                audioSource.mute = true;
+            }
+        }
     }
 }
